11acafccca235e74ecbb61b0cf20fdf2288b097f
eatfish <s1063680@gm.pu.edu.tw>
2020-06-08 13:45:37 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/s1063680/little_earth/littleearthgame.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.s1063680.little_earth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class littleearthgame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_littleearthgame);
+    }
+}
